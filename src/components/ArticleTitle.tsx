611807892cdd613242981dc45def2de1929fcf5b
Larufu1880 <t.yohei1880@gmail.com>
2020-11-03 18:26:12 +0900

    add ArticleTitle component

---
+++ src/components/ArticleTitle.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const Title = styled.h1`
+  font-size: 0.8rem;
+`;
+
+interface IProps {
+  title: string;
+}
+
+export const ArticleTitle: React.FC<IProps> = props => <Title>{props.title}</Title>;
