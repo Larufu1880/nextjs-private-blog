e33b4ebc8033b3ca4a1617a0a260a75d2d3fe1aa
Larufu1880 <t.yohei1880@gmail.com>
2021-05-17 00:40:06 +0900

    add footer and twitter icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,33 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import styled from 'styled-components';
 
-const PageFooter = styled.footer`
+const Header = styled.header`
+`
 
+const HeaderTitle = styled.h1`
+  font-size: 3.0rem;
+  text-align: center;
+`
+
+const HeaderLink = styled.a`
+  display: block;
+  text-decoration: none;
+  color: #cccccc;
+  
+  &:visited {
+    color: #cccccc;
+  }
+`
+
+const PageFooter = styled.footer`
+  text-align: center;
+  margin-top: 30px;
+`
+
+const TwitterIconLink = styled.a`
+  display: inline-block;
+  text-align: center;
+  margin: 0 auto;
 `
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
@@ -14,9 +39,12 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Header>
+        <HeaderTitle><HeaderLink href="/">コウテイペンギンの地団駄</HeaderLink></HeaderTitle>
+      </Header>
       <Component {...pageProps} />
       <PageFooter>
-        hogefuga
+        <TwitterIconLink href="/"><img src="/twitter.svg" alt="twitter" width="32px" height="32px" /></TwitterIconLink>
       </PageFooter>
     </>
   );
